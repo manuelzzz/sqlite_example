1b3cd24e3711da29397e0c09b1da1bdeb017e21f
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-13 22:19:52 -0300

    SQL structure

--- lib/database/sql_connection.dart
+++ lib/database/sql_connection.dart
@@ -8,7 +8,12 @@ class SqlConnection {
 
     await openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 3,
+      // executado toda vez que o banco for aberto
+      // sempre que for alterado
+      onConfigure: (db) async {
+        await db.execute('''''');
+      },
       // onCreate -> só é chamado no momento de criação do db
       // só é rodado quando o aplicativo é carregado a primeira vez
       onCreate: (db, version) {
@@ -25,10 +30,49 @@ class SqlConnection {
       },
       // chamado sempre que houver uma alteração no "version" sendo incremental
       // (version++)
-      onUpgrade: (db, oldVersion, newVersion) {},
+      onUpgrade: (db, oldVersion, newVersion) {
+        final batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200)
+            )
+          ''');
+
+          // batch.execute('''
+          // create table categoria(
+          //  id Integer primary key autoincrement,
+          //    nome varchar(200)
+          //  )
+          // ''');
+        }
+
+        // if (oldVersion == 2) {
+        //   batch.execute('''
+        //     create table categoria(
+        //       id Integer primary key autoincrement,
+        //       nome varchar(200)
+        //     )
+        //   ''');
+        // }
+
+        batch.commit();
+      },
       // chamado sempre que houver uma alteração no "version" sendo decremental
       // (version--)
-      onDowngrade: (db, oldVersion, newVersion) {},
+      onDowngrade: (db, oldVersion, newVersion) {
+        final batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute('''
+            drop table categoria
+          ''');
+        }
+
+        batch.commit();
+      },
     );
   }
 }
